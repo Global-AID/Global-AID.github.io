61ba33653bb1188be1e47fac0c489dd8cdf63faf
Obaidullah <58884054+Global-AID@users.noreply.github.com>
2022-06-18 14:10:08 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
 particlesJS('particles-js',
   
   {
